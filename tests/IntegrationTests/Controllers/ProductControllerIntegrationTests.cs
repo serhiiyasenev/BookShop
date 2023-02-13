3b21f003d8523e86c88b2b7b8baf15036c7d2d4e
serhii.yasenev@gmail.com <serhii.yasenev@gmail.com>
2023-02-13 22:58:22 +0200

    add IntegrationTests

---
+++ tests/IntegrationTests/Controllers/ProductControllerIntegrationTests.cs
@@ -0,0 +1,63 @@
+﻿using Api;
+using Api.Helpers;
+using BusinessLayer.Models.Inbound;
+using BusinessLayer.Models.Outbound;
+using Microsoft.eShopWeb.FunctionalTests.Web.Controllers;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace IntegrationTests.Controllers
+{
+    [TestFixture]
+    public class ProductControllerIntegrationTests
+    {
+        private readonly string _requestUri;
+        private readonly HttpClient _httpClient;
+        private readonly CustomWebApplicationFactory _factory;
+
+        public ProductControllerIntegrationTests()
+        {
+            _requestUri = "/product";
+            _factory = new CustomWebApplicationFactory();
+            _httpClient = _factory.CreateClient();
+        }
+
+        [Test]
+        public async Task PostProduct_Returns_NewlyCreatedProduct()
+        {
+            // Arrange
+            var productInbound = new ProductInbound
+            {
+                Name = $"The Three Musketeers {DateTime.UtcNow.Ticks}",
+                Description = $"You have likely heard of The Three Musketeers! {DateTime.UtcNow.Ticks}",
+                Author = $"Alexandre Dumas {DateTime.UtcNow.Ticks}",
+                Price = 12.56f,
+                ImageUrl = $"ftp://book.shop/{DateTime.UtcNow.Ticks}/image.jpg"
+            };
+
+            var content = JsonHelper.ToStringContent(productInbound);
+
+
+            // Act
+            var createdProduct = await (await _httpClient.PostAsync(_requestUri, content))
+                 .EnsureSuccessStatusCode().GetModelAsync<ProductOutbound>();
+
+            // Assert
+            Assert.IsNotNull(createdProduct.Id);
+            Assert.IsNull(createdProduct.BookingId);
+
+            Assert.AreEqual(productInbound.Name, createdProduct.Name);
+            Assert.AreEqual(productInbound.Description, createdProduct.Description);
+            Assert.AreEqual(productInbound.Author, createdProduct.Author);
+            Assert.AreEqual(productInbound.Price, createdProduct.Price);
+            Assert.AreEqual(productInbound.ImageUrl, createdProduct.ImageUrl);
+        }
+
+        [OneTimeTearDown]
+        public async Task OneTimeTearDown()
+        {
+            _httpClient.Dispose();
+            await _factory.DisposeAsync();
+        }
+    }
+}
---
+++ tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,51 @@
+﻿using Api;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopWeb.FunctionalTests.Web.Controllers
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+
+                // Create a new service provider.
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<EfCoreContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<EfCoreContext>();
+                    var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory>>();
+
+                    // Ensure the database is created.
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
